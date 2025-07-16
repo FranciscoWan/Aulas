e65d9fbd8ebb3b857a13a128fb38962946bf22e9
Francisco Wanderer <franciscowan@outlook.com>
2025-07-16 13:41:39 -0300

    Add javascript leveling lesson 1

---
+++ Nivelamento JS - Infinity/Aula 1 - Variáveis e condicionais/script.js
@@ -0,0 +1,69 @@
+// // Atividade 1
+// var nome = prompt("Digite seu nome: ") 
+// console.log(`Olá, o nome do usuário é ${nome}`)  
+// alert(`Olá, o nome do usuário é ${nome}`)
+
+// //  Atividade 2
+// var nome = prompt("Digite seu nome: ")
+// var idade = prompt("Digite sua idade: ")
+// alert(`O nome do usuário é ${nome}, e sua idade é ${idade}`)
+
+// // Atividade 3
+// var num1 = prompt("Digite um número: ")
+// var num2 = prompt("Digite outro número: ")
+
+// alert(num1==num2)
+
+// // Atividade 4
+// var num1 = Number(prompt("Digite o primeiro número: ")) // 5
+// var num2 = Number(prompt("Digite o segundo número: ")) // 0
+// var num3 = Number(prompt("Digite o terceiro número: ")) // 10
+
+// alert(num1<num2 && num1>num3 || num1>num2 && num1<num3)
+
+// // Atividade 5
+// var idade = Number(prompt("Digite sua idade: "))
+
+// if (idade>=18){
+//     alert("Maior de idade")
+// }
+// else if (idade<18){
+//     alert("Menor de idade")
+// }
+
+// // Atividade 6
+// var num = Number(prompt("Digite um número: "))
+
+// if (num%2==0){
+//     alert(`O número ${num} é par`)
+// }
+// else{
+//     alert(`O número ${num} é ímpar`)
+// }
+
+// // Atividade 7
+// var num = Number(prompt("Digite um número: "))
+
+// if (num>0){
+//     alert(`O número ${num} é positivo.`)
+// }
+// else if (num<0){
+//     alert(`O número ${num} é negativo.`)
+// }
+// else{
+//     alert(`O número digitado foi o ${num}`)
+// }
+
+// // Atividade 8
+// // media = (nota1+nota2+nota3)/3
+// var nota1 = Number(prompt("Digite a primeira nota: "))
+// var nota2 = Number(prompt("Digite a segunda nota: "))
+// var nota3 = Number(prompt("Digite a terceira nota: "))
+// var media = (nota1+nota2+nota3)/3
+
+// if (media>=7){
+//     alert(`Aluno aprovado com média ${media.toFixed(2)}.`)
+// }
+// else{
+//     alert(`Aluno reprovado com média ${media.toFixed(2)}.`)
+// }
