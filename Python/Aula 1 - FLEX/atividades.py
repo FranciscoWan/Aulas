b2e77388fb140ab042b85e7fd62bfde3e3e3e94c
Francisco Wanderer <franciscowan@outlook.com>
2025-08-19 22:33:19 -0300

    Add aula1 - Flex

---
+++ Python/Aula 1 - FLEX/atividades.py
@@ -0,0 +1,19 @@
+# Atividade 1 - 
+nome = str(input("Digite um nome: "))
+idade = int(input("Digite uma idade: "))
+print(f"O nome do usuário é {nome} e sua idade é {idade}.")
+
+# Atividade 2 - 
+base = float(input("Digite a base da figura: "))
+altura = float(input("Digite a altura da figura: "))
+area = base*altura
+print(f"A área da figura é {area:.2f}m².")
+
+# Atividade 3 -
+real = float(input("Digite o valor em R$: "))
+v_dolar = real/5.50
+print(f"R${real} reais equivalem a U${v_dolar:.2f} dólares")
+
+# Atividade 4 -
+num1 = int(input("Digite um número: "))
+print(num1%2==0)
---
+++ Python/Aula 1 - FLEX/aula1.py
@@ -0,0 +1,26 @@
+# saudacao = "Olá mundo belo"   # String ou str
+# idade = 22                    # inteiro ou int
+# altura = 1.72                 # ponto flutuante ou float
+# booleanos = True or False     # bool
+
+# print(type(saudacao))
+# print(type(idade))
+# print(type(altura))
+# print(type(booleanos))
+
+# Idade, altura e exiba os dados digitados
+
+# nome_usuario = str(input("Digite um nome: "))
+# idade = int(input("Digite sua idade: "))
+# altura = float(input("Digite sua altura: "))
+
+# print(f"O nome do usuário é {nome_usuario}, sua idade é {idade} anos e sua altura é, {altura} m.")
+
+
+# idade1 = int(input("Digite a idade 1:"))
+# idade2 = int(input("Digite a idade 2:"))
+
+# print(idade1>=idade2)
+
+
+
