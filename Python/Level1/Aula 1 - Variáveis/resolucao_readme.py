c464b371bd7dac5cc64f2cb9763b20e3e133cdc5
Francisco Wanderer <franciscowanderer@hotmail.com>
2025-10-16 14:30:23 -0300

    Add resolucao_readme.py

---
+++ Python/Level1/Aula 1 - Variáveis/resolucao_readme.py
@@ -0,0 +1,47 @@
+# Exercícios Readme.md
+
+# Exercício 1
+nome = str(input("Digite o nome do usuário: "))
+print(f"Bem vindo {nome}")
+
+# Exercício 2
+idade = int(input("Digite a idade do usuário: "))
+print(f"A idade do usuário é {idade}")
+
+# Exercício 3
+cidade_natal = str(input("Digite a sua cidade natal: "))
+print(f"A sua cidade natal é: {cidade_natal}")
+
+# Exercício 4
+comida_favorita = str(input("Digite a sua comida favorita: "))
+print(f"A sua comida favorita é: {comida_favorita}")
+
+# Exercício 5
+cor_preferida = str(input("Digite a sua cor preferida: "))
+print(f"A sua cor preferida é: {cor_preferida}")
+
+# Exercício 6
+filme_favorito = str(input("Digite o seu filme favorito: "))
+print(f"O seu filme favorito é: {filme_favorito}")
+
+# Exercício 7
+primeiro_nome = str(input("Digite o seu primeiro nome: "))
+sobrenome = str(input("Digite o seu sobrenome: "))
+print("Seu nome completo é: " + primeiro_nome + " " + sobrenome) # podemos concatenar ("juntar") strings com o operador + .
+print(f"Seu nome completo é: {primeiro_nome} {sobrenome}")
+
+# Exercício 8
+nome = str(input("Digite o seu nome: "))
+idade = int(input("Digite a sua idade: "))
+altura = float(input("Digite a sua altura em metros (ex: 1.75): "))  # Lembre da utilização de ponto para decimais ao em vez de vírgula
+print(f"Seu nome é {nome}, você tem {idade} anos e sua altura é {altura} metros.")
+
+# Exercício 9
+numero = int(input("Digite seu número favorito: "))
+print(f"O número favorito digitado foi: {numero}")
+
+# Exercício 10
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+cidade = str(input("Digite sua cidade: "))
+print(f"Olá, {nome}! Você tem {idade} anos e mora em {cidade}.")
